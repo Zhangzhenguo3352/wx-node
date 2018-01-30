3b705abdb4f543fdd8b03508a8255cebc570568c
Zhangzhenguo3352 <Zhangzhenguo3352@163.com>
2018-01-30 21:53:33 +0800

    no message

--- server.js
+++ server.js
@@ -18,42 +18,47 @@ app.get('/', (req, res) => {
       // res.send({'8999':111})
     
     // 第一种 get 方式
-    // // 这里去调用了  另外一个接口， 就可以认为是调用了 微信的接口  
-    // request('http://localhost:8888/', function (error, response, body) {
-    //   if (!error && response.statusCode == 200) {
-    //     console.log('body',body) // Show the HTML for the baidu homepage.
-    //   }
-    //   res.send({'8999':111})
-    // })
-    
-    // 第二种 post 方式
-    request({
-        url: 'http://localhost:8888/',
-        method: "POST",
-        json: true,
-        headers: {
-            "content-type": "application/json",
-        },
-        data: {
-            js_code: req.query,
-            appid: 'wxdb0b83006fe07fa6',
-            secret: 'd28ba63b80b40f6a970ab6c15cc2e034',
-            grant_type: 'authorization_code'
-        },
-        // body: JSON.stringify({name:1})
-        body: {"aaa": "111"}
-    }, function(error, response, body) {
-        console.log('response ',response)
+    // 这里去调用了  另外一个接口， 就可以认为是调用了 微信的接口  
+    request(`https://api.weixin.qq.com/sns/jscode2session?appid=wxdb0b83006fe07fa6&secret=d28ba63b80b40f6a970ab6c15cc2e034&js_code=${req.query.code}&grant_type=authorization_code`, function (error, response, body) {
+            console.log('response ',response)
         console.log('body ',body)
         console.log('!error ',error)
         console.log('response.statusCode == 200',response.statusCode == 200)
         console.log('!error && response.statusCode == 200',!error && response.statusCode == 200)
-        if (!error && response.statusCode == 200) {
-            console.log('body',body)
+      if (!error && response.statusCode == 200) {
+        console.log('body',body) // Show the HTML for the baidu homepage.
+      }
+      res.send({'8999':111})
+    })
+    
+    // 第二种 post 方式
+    // request({
+    //     url: 'http://localhost:8888/',
+    //     method: "POST",
+    //     json: true,
+    //     headers: {
+    //         "content-type": "application/json",
+    //     },
+    //     data: {
+    //         js_code: req.query,
+    //         appid: 'wxdb0b83006fe07fa6',
+    //         secret: 'd28ba63b80b40f6a970ab6c15cc2e034',
+    //         grant_type: 'authorization_code'
+    //     },
+    //     // body: JSON.stringify({name:1})
+    //     body: {"aaa": "111"}
+    // }, function(error, response, body) {
+    //     console.log('response ',response)
+    //     console.log('body ',body)
+    //     console.log('!error ',error)
+    //     console.log('response.statusCode == 200',response.statusCode == 200)
+    //     console.log('!error && response.statusCode == 200',!error && response.statusCode == 200)
+    //     if (!error && response.statusCode == 200) {
+    //         console.log('body',body)
 
-        }
-        res.send({'8999':111})
-    }); 
+    //     }
+    //     res.send({'8999':111})
+    // }); 
 })
 
 
