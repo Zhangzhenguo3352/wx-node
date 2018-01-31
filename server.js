e5d27de073e403ef862ec4d47b7bfdf568c467b2
Zhangzhenguo3352 <Zhangzhenguo3352@163.com>
2018-01-31 10:41:45 +0800

    no message

--- server.js
+++ server.js
@@ -8,15 +8,34 @@ var path = require('path')
 var fs = require('fs')
 var bodyParser = require('body-parser');
 var request = require('request');
+var md5 = require('md5');
 
 
 var app_id = 'wxdb0b83006fe07fa6';
 var app_secret = 'd28ba63b80b40f6a970ab6c15cc2e034'
 
+// 生成 随机 32位的字符串
+function strFn(A_length){  
+    var data=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];  
+              
+    var result="";  
+    for(var i=0;i<A_length;i++){  
+        var r=Math.floor(Math.random()*62);     //取得0-62间的随机数，目的是以此当下标取数组data里的值！
+        result+=data[r];        //输出20次随机数的同时，让rrr加20次，就是20位的随机字符串了。
+    }  
+    return result;
+} 
+// 用三组字符串，进行加密
+var str = 'aaaabbsssbsaewHHHSDKkflwklkclwl3434123d'
+// 盐（自己写 一段自己都不认识的 字母）
+var yan = 'JJKkkkfkk9c949vjJJJjvwi4izk  kk';
+
+
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.get('/', (req, res) => {
     console.log('req.body',req.query.code)
+    console.log('md5',md5(md5(md5(`${strFn(32)}${str}${yan}`))))
       // res.send({'8999':111})
     
     // 第一种 get 方式
